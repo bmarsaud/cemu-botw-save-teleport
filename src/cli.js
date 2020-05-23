ea69dd07361eced6a25fd4517bc23ad03fdc59b6
Bastien Marsaud <bastien@mouseover.fr>
2020-05-23 18:42:12 +0200

    feat: make script CLI

---
+++ src/cli.js
@@ -0,0 +1,71 @@
+const arg = require('arg');
+const inquirer = require('inquirer');
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--savePath': String,
+            '--posX': Number,
+            '--posY': Number,
+            '--posZ': Number,
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+
+    return {
+        savePath: args['--savePath'],
+        posX: args['--posX'],
+        posY: args['--posY'],
+        posZ: args['--posZ'],
+    };
+}
+
+async function promptMissingOptions(options) {
+    let questions = [];
+
+    if(!options.savePath) {
+        questions.push({
+            name: 'filePath',
+            message: 'BOTW save file path to edit:'
+        });
+    }
+
+    if(!options.posX) {
+        questions.push({
+            name: 'posX',
+            message: 'Player X position:',
+            type: 'number'
+        });
+    }
+
+    if(!options.posX) {
+        questions.push({
+            name: 'posY',
+            message: 'Player Y position:',
+            type: 'number'
+        });
+    }
+
+    if(!options.posZ) {
+        questions.push({
+            name: 'posZ',
+            message: 'Player Z position:',
+            type: 'number'
+        });
+    }
+
+    let answers = await inquirer.prompt(questions);
+    return {...options, ...answers};
+}
+
+async function cli(args) {
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptMissingOptions(options);
+    console.log(options);
+}
+
+module.exports = {
+    cli: cli
+}
